5dafd4de3627bf1c1e6629f09629e8dad7849227
thanetsuwanapirom <s6406022620029@email.kmutnb.ac.th>
2022-11-02 00:29:42 +0700

    up

--- Pro/web.py
+++ Pro/web.py
@@ -1,11 +1,24 @@
 from flask import Flask,render_template
 from flask_sqlalchemy import SQLAlchemy
-
+import psycopg2
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('test.html')
+    connection = psycopg2.connect(user='webadmin',
+                                    password='VCNtps41396',
+                                    host='node37019-thanet.proen.app.ruk-com.cloud',
+                                    port='11235',
+                                    database='project')
+    cursor = connection.cursor()
+
+    select_copper = 'select * from copper'
+
+    cursor.execute(select_copper)
+    Data = cursor.fetchall()
+
+
+    return render_template('test.html',DataCopper=Data)
 
 if __name__  == '__main__':
     app.run(host='0.0.0.0',port=80,debug=True)
