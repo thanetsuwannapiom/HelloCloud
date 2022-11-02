7aae4e1f85fece7fae0ac170344f7e080c535bdd
thanetsuwanapirom <s6406022620029@email.kmutnb.ac.th>
2022-11-02 19:03:20 +0700

    up

--- Pro/web.py
+++ Pro/web.py
@@ -20,6 +20,11 @@ def index():
 
     return render_template('test.html',DataCopper=Data)
 
+@app.route('/home')
+def home():
+
+    return render_template('home.html')
+
 if __name__  == '__main__':
     app.run(host='0.0.0.0',port=80,debug=True)
 
